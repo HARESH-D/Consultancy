d801d77480ffb7c856776d47f0376ae29cb9b520
hareshdinakar <73981964+HARESH-D@users.noreply.github.com>
2022-04-25 22:03:28 +0530

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -2,12 +2,25 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import tensorflow as tf
+from tensorflow import keras
+from pandas import concat
+from pandas import Series
+from pandas import DataFrame
+from pandas import read_csv
+from pandas import datetime
 import sklearn.metrics as metrics
+from tensorflow.keras.models import Sequential
 from matplotlib import pyplot as plt
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Dense, TimeDistributed, LSTM, Bidirectional, GRU
 
-df1 = pd.read_csv(r"Data_set_1.csv")
-df2 = pd.read_csv(r"Data_set_2.csv")
+df1 = pd.read_csv(r"Data_set_1.csv", parse_dates=[1], encoding='utf-8')
+df2 = pd.read_csv(r"Data_set_2.csv", parse_dates=[1], encoding='utf-8')
 df = pd.concat([df1, df2]).reset_index(drop=True)
 df_product_count = pd.DataFrame([])
 df_product_count['product'] = df['product']
@@ -16,7 +29,198 @@ print(df_product_count['freq'])
 df_product_count = df_product_count.drop_duplicates(subset=["product"], keep='first', ignore_index=True)
 print(df_product_count)
 df_product_count = df_product_count.sort_values(by=['freq'], ascending=False)
-df_product_count.to_csv("productlist.csv")
+# df_product_count.to_csv("productlist.csv")
 print(df_product_count)
 product_list = df_product_count["product"].values
 print("No of Parts: ", len(product_list))
+
+df_preprocessed = pd.DataFrame(columns=["Product", "quantity"])
+
+# for parts in product_list:
+df_prod = df.loc[df['product'] == "S4402E0C31"]
+df_semi_prod_month = df_prod.groupby(pd.Grouper(key='date', freq='D')).sum().reset_index()
+# # print(len(df_prod_month))
+# if len(df_semi_prod_month) >= 130:
+df1 = df_semi_prod_month
+values = df1["quantity"].values
+
+print(values)
+print(len(values))
+
+
+# convert time series into supervised learning problem
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
+# create a differenced series
+def difference(dataset, interval=1):
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return Series(diff)
+
+
+# transform series into train and test sets for supervised learning
+def prepare_data(series, n_test, n_lag, n_seq):
+    # extract raw values
+    raw_values = series.values
+    # transform data to be stationary
+    diff_series = difference(raw_values, 1)
+    diff_values = diff_series.values
+    diff_values = diff_values.reshape(len(diff_values), 1)
+    # rescale values to -1, 1
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    scaled_values = scaler.fit_transform(diff_values)
+    scaled_values = scaled_values.reshape(len(scaled_values), 1)
+    # transform into supervised learning problem X, y
+    supervised = series_to_supervised(scaled_values, n_lag, n_seq)
+    supervised_values = supervised.values
+    # split into train and test sets
+    train, test = supervised_values[0:-n_test], supervised_values[-n_test:]
+    return scaler, train, test
+
+
+# fit an LSTM network to training data
+def fit_lstm(train, n_lag, n_seq, n_batch, nb_epoch, n_neurons):
+    # reshape training into [samples, timesteps, features]
+    X, y = train[:, 0:n_lag], train[:, n_lag:]
+    X = X.reshape(X.shape[0], 1, X.shape[1])
+    # design network
+    model = Sequential()
+    model.add(LSTM(n_neurons, return_sequences=True, batch_input_shape=(n_batch, X.shape[1], X.shape[2])))
+    model.add(Bidirectional(LSTM(n_neurons, stateful=True)))
+    model.add(Dense(y.shape[1]))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    # fit network
+    # for i in range(nb_epoch):
+    # history = model.fit(X, y, epochs=1, batch_size=n_batch, verbose=2, shuffle=False)
+    model.fit(X, y, epochs=70, batch_size=n_batch, verbose=2, shuffle=False)
+        # print(history)
+    # model.reset_states()
+    return model
+
+
+# make one forecast with an LSTM,
+def forecast_lstm(model, X, n_batch):
+    # reshape input pattern to [samples, timesteps, features]
+    X = X.reshape(1, 1, len(X))
+    # make forecast
+    forecast = model.predict(X, batch_size=n_batch)
+    # convert to array
+    return [x for x in forecast[0, :]]
+
+
+# evaluate the persistence model
+def make_forecasts(model, n_batch, train, test, n_lag, n_seq):
+    forecasts = list()
+    for i in range(len(test)):
+        X, y = test[i, 0:n_lag], test[i, n_lag:]
+        # make forecast
+        forecast = forecast_lstm(model, X, n_batch)
+        # store the forecast
+        forecasts.append(forecast)
+    return forecasts
+
+
+# invert differenced forecast
+def inverse_difference(last_ob, forecast):
+    # invert first forecast
+    inverted = list()
+    inverted.append(forecast[0] + last_ob)
+    # propagate difference forecast using inverted first value
+    for i in range(1, len(forecast)):
+        inverted.append(forecast[i] + inverted[i - 1])
+    return inverted
+
+
+# inverse data transform on forecasts
+def inverse_transform(series, forecasts, scaler, n_test):
+    inverted = list()
+    for i in range(len(forecasts)):
+        # create array from forecast
+        forecast = array(forecasts[i])
+        forecast = forecast.reshape(1, len(forecast))
+        # invert scaling
+        inv_scale = scaler.inverse_transform(forecast)
+        inv_scale = inv_scale[0, :]
+        # invert differencing
+        index = len(series) - n_test + i - 1
+        last_ob = series.values[index]
+        inv_diff = inverse_difference(last_ob, inv_scale)
+        # store
+        inverted.append(inv_diff)
+    return inverted
+
+
+# evaluate the RMSE for each forecast time step
+def evaluate_forecasts(test, forecasts, n_lag, n_seq):
+    for i in range(n_seq):
+        actual = [row[i] for row in test]
+        predicted = [forecast[i] for forecast in forecasts]
+        rmse = sqrt(mean_squared_error(actual, predicted))
+        print('t+%d RMSE: %f' % ((i + 1), rmse))
+
+
+# plot the forecasts in the context of the original dataset
+def plot_forecasts(series, forecasts, n_test):
+    # plot the entire dataset in blue
+    pyplot.plot(series.values)
+    # plot the forecasts in red
+    for i in range(len(forecasts)):
+        off_s = len(series) - n_test + i - 1
+        off_e = off_s + len(forecasts[i]) + 1
+        xaxis = [x for x in range(off_s, off_e)]
+        yaxis = [series.values[off_s]] + forecasts[i]
+        pyplot.plot(xaxis, yaxis, color='red')
+    # show the plot
+    pyplot.show()
+
+
+n_lag = 1
+n_seq = 5
+n_test = 495
+n_epochs = 70
+n_batch = 1
+n_neurons = 64
+
+# series = values
+series = pd.DataFrame(values)
+
+# print(type(series))
+
+scaler, train, test = prepare_data(series, n_test, n_lag, n_seq)
+print
+# fit model
+model = fit_lstm(train, n_lag, n_seq, n_batch, n_epochs, n_neurons)
+# make forecasts
+forecasts = make_forecasts(model, n_batch, train, test, n_lag, n_seq)
+# inverse transform forecasts and test
+forecasts = inverse_transform(series, forecasts, scaler, n_test+2)
+actual = [row[n_lag:] for row in test]
+actual = inverse_transform(series, actual, scaler, n_test+2)
+# evaluate forecasts
+evaluate_forecasts(actual, forecasts, n_lag, n_seq)
+# plot forecasts
+plot_forecasts(series, forecasts, n_test+2)
